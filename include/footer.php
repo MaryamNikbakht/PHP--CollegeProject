d5bbfc278e07c04a0334fc5d3e04123d7e1c6da9
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2017-09-18 20:47:16 +0200

    add Script-Tags

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,9 @@
 <footer class="container-fluid text-center">
 
   <p>Copyright(c) OnlineWorld.<span>Designed by: <a href="https://www.linkedin.com/in/maryam-nikbakht?trk=nav_responsive_tab_profile_pic">@Maryam-Nikbakht</a></span></p>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!--   <script type="text/javascript" src="test.js"></script> -->
 </footer>
-
-
-
-
-
 </body>
 </html>
