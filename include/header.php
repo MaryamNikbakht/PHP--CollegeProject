4499f38e22f7cfc4cf7d05ac07d4eae7f3aa34db
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2017-09-18 20:46:32 +0200

    remove Script-Tags

--- include/header.php
+++ include/header.php
@@ -6,12 +6,7 @@
   <title>Online Car Shoppers</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!--   <script type="text/javascript" src="test.js"></script> -->
- 
- 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
 
   <style>
     /* Remove the navbar's default margin-bottom and rounded borders */ 
